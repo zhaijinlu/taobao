a8ec38977792c1db969f433de91639197cd03856
zhaijinlu <1375716717@qq.com>
2017-08-31 08:58:42 +0800

    08-13 建立工程

---
+++ TaoBao/app/src/main/java/com/bwie/app/activity/MainActivity.java
@@ -0,0 +1,18 @@
+package com.bwie.app.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bwie.app.R;
+
+/**
+ * 第一次创建
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
