b837db26d15a29265de9f52aea726e0e9e18cd1d
zhaijinlu <1375716717@qq.com>
2017-09-19 16:43:34 +0800

    答辩前

--- TaoBao/app/src/main/java/com/bwie/app/activity/MainActivity.java
+++ TaoBao/app/src/main/java/com/bwie/app/activity/MainActivity.java
@@ -1,5 +1,8 @@
 package com.bwie.app.activity;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,10 +12,27 @@ import com.bwie.app.R;
  * 第一次创建
  */
 public class MainActivity extends AppCompatActivity {
-
+    private int i=2;
+    private Handler handler=new Handler();
+    Runnable runnable=new Runnable() {
+        @Override
+        public void run() {
+            if(i>0){
+                i--;
+                handler.postDelayed(runnable,800);
+            }else {
+                Intent intent=new Intent(MainActivity.this,FirstActivity.class);
+                startActivity(intent);
+                handler.removeCallbacks(runnable);
+                finish();
+            }
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //四秒自动跳转
+        handler.postDelayed(runnable,800);
     }
 }
